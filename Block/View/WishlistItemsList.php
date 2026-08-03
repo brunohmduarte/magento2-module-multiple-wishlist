4fe572ea62a09c9bc846c7fb93abc0c2ff4f7b61
Bruno Duarte <bruno.duarte@bis2bis.com.br>
2026-08-03 09:35:16 -0300

    🔨 Implementing view wishlist page

---
+++ Block/View/WishlistItemsList.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @category BrunoDuarte
+ * @package BrunoDuarte_MultipleWishlist
+ * @copyright Copyright (c) 2026 BrunoDuarte
+ */
+declare(strict_types=1);
+
+namespace BrunoDuarte\MultipleWishlist\Block\View;
+
+use Magento\Framework\View\Element\Template;
+
+class WishlistItemsList extends Template
+{
+
+}
